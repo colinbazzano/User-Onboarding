10baea7960e3beddfa36791599f5232d27ca720b
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-17 19:10:31 -0400

    styling changes using styled components

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 import OnboardForm from './components/Form';
+
+const HeadingTitle = styled.h1`
+  font-size: 2rem;
+`;
+
 
 function App() {
   return (
     <div className="App">
+      <HeadingTitle>Meet Our Team</HeadingTitle>
       <OnboardForm />
     </div>
   );
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,7 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { withFormik, Formik, Form, Field } from 'formik';
+import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components';
+
+const MainForm = styled(Form)`
+    width: 30%;
+    height: auto;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+`;
+
+const TopField = styled(Field)`
+    font-size: 1rem;
+    margin: 2%;
+`;
+const SelectField = styled(Field)`
+    font-size: 1rem;
+    margin: 2% auto;
+`;
+const TermsLabel = styled.label`
+    font-size: 1rem;
+    margin: 1%;
+`;
+const SubmitButton = styled.button`
+    background-color: #007bff;
+    font-size: 1.25rem;
+    color: white;
+    width: 40%;
+    height: 30px;
+    margin: 4% auto;
+    border-radius: .25rem;
+`;
+
 
 const OnboardForm = ({values, touched, errors, status }) => {
     const [users, setUsers] = useState([])
@@ -10,36 +43,36 @@ const OnboardForm = ({values, touched, errors, status }) => {
     },[status])
     return(
         <div className='onboard-form'>
-            <Form>
-                <Field type='text' name='name' placeholder='Name'/>
+            <MainForm>
+                <TopField type='text' name='name' placeholder='Name'/>
                 {touched.name && errors.name && (
                     <p className='error'>{errors.name}</p>
                 )}
-                <Field type='email' name='email' placeholder='Email'/>
+                <TopField type='email' name='email' placeholder='Email'/>
                 {touched.email && errors.email && (
                     <p className='error'>{errors.email}</p>
                 )}
-                <Field type='password' name='password' placeholder='Password' />
+                <TopField type='password' name='password' placeholder='Password' />
                 {touched.password && errors.password && (
                     <p className='error'>{errors.password}</p>
                 )}
-                <Field component='select' className='role-select' name='role'>
+                <SelectField component='select' className='role-select' name='role'>
                     <option>Choose your role</option>
-                    <option value='frontend'>Frontend</option>
-                    <option value='backend'>Backend</option>
-                    <option value='fullstack'>Fullstack</option>
-                    <option value='other'>Other</option>
-                </Field>
+                    <option value='Frontend'>Frontend</option>
+                    <option value='Backend'>Backend</option>
+                    <option value='Fullstack'>Fullstack</option>
+                    <option value='Other'>Other</option>
+                </SelectField>
                 {touched.role && errors.role && (
                     <p className='error'>{errors.role}</p>
                 )}
-                <label className='checkbox-container'>
+                <TermsLabel className='checkbox-container'>
                     {' '}
                     Terms of Service
                     <Field type='checkbox' name='terms' checked={values.terms}/>
-                </label>
-                <button type='submit'>Submit</button>
-            </Form>
+                </TermsLabel>
+                <SubmitButton type='submit'>Submit</SubmitButton>
+            </MainForm>
             {users.map(user => (
                 <ul key={user.id}>
                     <li>Name: {user.name}</li>
@@ -65,7 +98,7 @@ const FormikForm = withFormik({
         name: Yup.string().required('You must have a name, no?'),
         email: Yup.string().required('Most people these days have emails, what is yours?'),
         password: Yup.string().required('Passwords keep you safe, so please enter one'),
-        role: Yup.string().oneOf(['frontend', 'backend', 'fullstack', 'other'])
+        role: Yup.string().oneOf(['Frontend', 'Backend', 'Fullstack', 'Other'])
         .required('Please select a role')
     }),
     handleSubmit(values, {setStatus, resetForm}) {
